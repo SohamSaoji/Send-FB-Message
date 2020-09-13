495206f8b161fc3f2a0299c41cac368bc493861d
Soham Saoji <programmer.soham@gmail.com>
2020-09-13 22:11:02 +0530

    code

---
+++ sendFBmessage.py
@@ -0,0 +1,20 @@
+# pip install fbchat
+
+import fbchat
+from getpass import getpass
+
+username=str((input("Username: ")))
+client = fbchat.Client(username, getpass())
+
+no_of_friends = int(input("Number of friends: "))
+
+for i in range(no_of_friends):
+    name = str(input("Name: "))
+    friends = client.getUsers(name)
+    
+    # return a list of names
+    friend = friends[0]
+    msg = str(input("Message: "))
+    sent = client.send(friend.uid, msg)
+    if sent:
+        print("Message Send!")
